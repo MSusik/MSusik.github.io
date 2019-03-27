8811643f11932957557dd3c384836d4f578ea841
Mateusz Susik <msusik@sigmoidal.io>
2019-03-27 10:20:12 +0100

    no copying
    Signed-off-by: Mateusz Susik <msusik@sigmoidal.io>

--- copyFiles.py
+++ copyFiles.py
@@ -4,5 +4,5 @@ import shutil
 BACKS = 'src/assets/backs'
 
 hash = os.listdir('dist/static/img')[0].split('.')[1]
-for file in os.listdir(BACKS):
-    shutil.copyfile(BACKS + '/' + file, 'dist/static/css/static/img/' + file)
+#for file in os.listdir(BACKS):
+#    shutil.copyfile(BACKS + '/' + file, 'dist/static/css/static/img/' + file)
